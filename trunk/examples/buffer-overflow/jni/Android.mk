35bb785d43ef4eaeb884aed4e23ff79ef2500784
marcelkolodziejczyk@gmail.com <marcelkolodziejczyk@gmail.com@97599784-e0b2-fd32-969a-e803eb497a9f>
2013-09-07 15:15:14 +0000

    porzadki w przykladach

--- trunk/examples/buffer-overflow/jni/Android.mk
+++ trunk/examples/buffer-overflow/jni/Android.mk
@@ -2,8 +2,8 @@ LOCAL_PATH := $(call my-dir)
  
 include $(CLEAR_VARS)
   
-LOCAL_MODULE    := stack-buffer-overflow
-LOCAL_SRC_FILES := stack-buffer-overflow.c
+LOCAL_MODULE    := buffer-overflow
+LOCAL_SRC_FILES := buffer-overflow.c
 LOCAL_CFLAGS += -g -ggdb -fno-stack-protector 
 LOCAL_DISABLE_NO_EXECUTE := true
 LOCAL_DISABLE_RELRO := true
